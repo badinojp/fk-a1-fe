fcdec16b4311b438fddd5a36c01ab54866d78a6e
Juampi Badino <juanpablobadino6@gmail.com>
2023-08-23 09:34:44 -0300

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import {
+  Box,
+  Container
+} from '@chakra-ui/react';
+
+const LogIn = () => {
+  return (
+    <Container>
+      <Box>Tatix</Box>
+      <Box>Tu colección digital de películas y series</Box>
+      <Box>Email</Box>
+      <Box>Contraseña</Box>
+      <Box>Recuperar contraseña</Box>
+      <Box>Términos y condiciones</Box>
+      <Box>Restablecer contraseña</Box>
+      <Box>Registrarme</Box>
+Iniciar sesión
+    </Container>
+  )
+}
+
+export default LogIn;
